319a39766a0fc6d2ced52ef7cbc8af4616583db5
inanadea <in@anadeainc.com>
2022-09-15 12:33:06 +0300

    Initial Commit

---
+++ Nasa APOD/Nasa_APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nasa_APODApp.swift
+//  Nasa APOD
+//
+//  Created by Ivan Nazarov on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
